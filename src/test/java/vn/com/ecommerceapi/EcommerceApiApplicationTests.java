4f4d8d6005538b1e8b8796dc385ac5dadd2de6cb
HopNV <hopnv.ous@mcredit.com.vn>
2024-05-27 08:35:16 +0700

    Init Server

---
+++ src/test/java/vn/com/ecommerceapi/EcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.ecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
